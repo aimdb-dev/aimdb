efd9fc0423e5ceece2422dd922688db09790ddba
Alexander Schnörch <alexander.schnoerch@outlook.com>
2026-06-11 19:12:37 +0000

    refactor: clean up import statements in KNX connector demo

--- examples/embassy-knx-connector-demo/src/main.rs
+++ examples/embassy-knx-connector-demo/src/main.rs
@@ -42,9 +42,7 @@ extern crate alloc;
 
 use aimdb_core::remote::SecurityPolicy;
 use aimdb_core::{AimDbBuilder, RecordKey, RuntimeContext};
-use aimdb_embassy_adapter::{
-    EmbassyAdapter, EmbassyBufferType, EmbassyRecordRegistrarExtCustom,
-};
+use aimdb_embassy_adapter::{EmbassyAdapter, EmbassyBufferType, EmbassyRecordRegistrarExtCustom};
 use aimdb_knx_connector::dpt::{Dpt1, Dpt9, DptDecode, DptEncode};
 use aimdb_knx_connector::embassy_client::KnxConnectorBuilder;
 use aimdb_serial_connector::embassy_transport::SerialServer;
